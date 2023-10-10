1858744b06bf514c2d5021126391d62165bd9056
Crowdbotics <team@crowdbotics.com>
2023-10-10 19:22:58 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "yellow-salad-43860",
+  url: "https://yellow-salad-43860.botics.co",
+  api: "https://yellow-salad-43860.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
